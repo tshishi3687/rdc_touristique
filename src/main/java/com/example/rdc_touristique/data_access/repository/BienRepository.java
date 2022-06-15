456d9812a9f279d2a391eb721d189ba2531c5425
tshishi3687 <tshishi@outlook.be>
2022-06-16 00:04:27 +0200

    update 16 06 22 v1

--- src/main/java/com/example/rdc_touristique/data_access/repository/BienRepository.java
+++ src/main/java/com/example/rdc_touristique/data_access/repository/BienRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 public interface BienRepository extends JpaRepository<Bien, Integer> {
     List<Bien> findAllByAppartientAndModeActiveFalse(Personne personne);
 
+    List<Bien> findByModeActiveOrderByIdDesc();
+
     List<Bien> findByType_NomContainingAndCoordonnee_Ville_NomVilleContainingAndCoordonnee_Ville_Province_NomprovinceContainingAndModeActiveTrueOrderByIdDesc(String type_nom, String coordonnee_ville_nomVille, String coordonnee_ville_province_nomprovince, Pageable pageable);
     int countByType_NomContainingAndCoordonnee_Ville_NomVilleContainingAndCoordonnee_Ville_Province_NomprovinceContainingAndModeActiveTrue(String type_nom, String coordonnee_ville_nomVille, String coordonnee_ville_province_nomprovince);
 }
--- src/test/java/com/example/rdc_touristique/RdcTouristiqueApplicationTests.java
+++ src/test/java/com/example/rdc_touristique/RdcTouristiqueApplicationTests.java
@@ -1,31 +1,13 @@
 package com.example.rdc_touristique;
 
-import com.example.rdc_touristique.business.dto.BienVuDTO;
-import com.example.rdc_touristique.business.mapper.Mapper;
-import com.example.rdc_touristique.data_access.entity.Bien;
-import com.example.rdc_touristique.data_access.repository.BienRepository;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-@SpringBootTest
+//@SpringBootTest
 class RdcTouristiqueApplicationTests {
 
-
-	@Autowired
-	private BienRepository bienRepository;
-	@Autowired
-	private Mapper<BienVuDTO, Bien> bienVuDTOBienMapper;
-
 //	@Test
-//	void contextLoads() {
-//		List<BienVuDTO> list = bienRepository.findByModeActiveOrderByIdDesc().stream()
-//				.map(bienVuDTOBienMapper::toDTO)
-//				.collect(Collectors.toList());
-//		System.out.println(list);
-//	}
+	void contextLoads() {
+	}
 
 }
