3e75780f2257a116d2ac7835763f4d0dd68ddd25
tshishi3687 <tshishi@outlook.be>
2022-06-16 00:23:31 +0200

    update 16 06 22 v2

--- src/test/java/com/example/rdc_touristique/RdcTouristiqueApplicationTests.java
+++ src/test/java/com/example/rdc_touristique/RdcTouristiqueApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.rdc_touristique;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 //@SpringBootTest
 class RdcTouristiqueApplicationTests {
